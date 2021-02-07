ab20d5f315cc442632d4b533ee49f4235ec14d4d
Kris Anderson <1562427+kris-anderson@users.noreply.github.com>
2021-02-06 22:09:37 -0800

    awsprofile v0.1.1

--- Formula/awsprofile.rb
+++ Formula/awsprofile.rb
@@ -1,14 +1,18 @@
 class Awsprofile < Formula
   desc "Utility that allows for easy switching and management of AWS CLI profiles"
   homepage "https://github.com/kris-anderson/awsprofile"
-  url "https://github.com/kris-anderson/awsprofile/releases/download/v0.1.0/awsprofile.tar.gz"
-  sha256 "b9b7b9520612e604bb0938b3054610c86c3c3e840ced9b095c7bde660466ccff"
+  url "https://github.com/kris-anderson/awsprofile/releases/download/v0.1.1/awsprofile.tar.gz"
+  sha256 "647b96d0a3bbbe8678d06b82ccb926d196409656aa11e995d6a9ff954246ab9d"
   license "MIT"
   head "git@github.com:kris-anderson/awsprofile.git", branch: "main"
 
   def install
     bin.install "awsprofile"
     man1.install "docs/awsprofile.1"
+
+    bash_completion.install "completions/bash/awsprofile"
+    zsh_completion.install "completions/zsh/_awsprofile"
+    fish_completion.install "completions/fish/awsprofile.fish"
   end
 
   test do
