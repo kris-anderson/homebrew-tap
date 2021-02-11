d28c0a997d56ff9214baba454b52729c4b0d73bd
Kris Anderson <1562427+kris-anderson@users.noreply.github.com>
2021-02-11 00:22:59 -0800

    awsprofile v0.1.3

--- Formula/awsprofile.rb
+++ Formula/awsprofile.rb
@@ -1,8 +1,8 @@
 class Awsprofile < Formula
   desc "Utility that allows for easy switching and management of AWS CLI profiles"
   homepage "https://github.com/kris-anderson/awsprofile"
-  url "https://github.com/kris-anderson/awsprofile/releases/download/v0.1.2/awsprofile.tar.gz"
-  sha256 "6d07446aab280dda4065664a962d10dc061bd7f12731daec5f9f258bb351fa77"
+  url "https://github.com/kris-anderson/awsprofile/releases/download/v0.1.3/awsprofile.tar.gz"
+  sha256 "40ee365810b94885cf0d897826c17e0746fcaa7c9b19fb7ea9274e7fbd38df0b"
   license "MIT"
   head "git@github.com:kris-anderson/awsprofile.git", branch: "main"
 
